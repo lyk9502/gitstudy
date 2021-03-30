5773a2c49a629d8780bdbdbc653a32f5583159e8
lyk9502 <lyk9502@163.com>
2021-03-31 02:06:50 +0800

    #ATSP-698第一次提交#

---
+++ src/main/java/com/example/gitdemo01/Gitdemo01Application.java
@@ -0,0 +1,13 @@
+package com.example.gitdemo01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gitdemo01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Gitdemo01Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gitdemo01/Gitdemo01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitdemo01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gitdemo01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
